4f4c2e04ef96ca2c262ceb46d01c24a3806537cb
Johnnytayy <e0303321@u.nus.edu>
2025-01-15 17:41:12 +0800

    Initial commit

---
+++ server.R
@@ -0,0 +1,34 @@
+library(shiny)
+
+function(input, output, session) {
+  output$nomogramPlot <- renderPlot({
+    par(mar = c(2, 2, 1, 1))
+    plot(
+      nom,
+      xfrac = 0.1,
+      cex.var = 0.7,
+      cex.axis = 0.7,
+      lineheight = 1,
+      main = "Nomogram for Predicting CVD"
+    )
+  })
+  
+  observeEvent(input$calculate, {
+    new_data <- data.frame(
+      sex = factor(input$sex, levels = c("Male", "Female")),
+      age = input$age,
+      Nicotine = factor(input$Nicotine, levels = c("Never", "Current", "Former")),
+      Cholesterol = factor(input$Cholesterol, levels = c("No", "Yes")),
+      DM = factor(input$DM, levels = c("No", "Yes")),
+      BPQ050A = factor(input$BPQ050A, levels = c("No", "Yes")),
+      ACES_with_complexity_combined = factor(input$ACES_with_complexity_combined, 
+                                             levels = c("Other", "Stage III", "Stage IV", "Edentulous"))
+    )
+    
+    risk <- predict(nom_model, new_data, type = "fitted")
+    
+    output$predictedProbability <- renderText({
+      paste("Predicted Probability of CVD:", round(risk * 100, 2), "%")
+    })
+  })
+}
---
+++ ui.R
@@ -0,0 +1,37 @@
+# set wd first
+# setwd("C:/Users/johnt/Dropbox/John/Nomogram and Life8/NomogramCVD/external validation code/my shiny app")
+# runApp()
+
+
+library(shiny)
+
+fluidPage(
+  titlePanel("Interactive Nomogram for CVD Identification"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      h4("Adjust Predictor Variables"),
+      selectInput("sex", "Sex", choices = c("Male" = "Male", "Female" = "Female"), selected = "Male"),
+      sliderInput("age", "Age", min = 20, max = 90, value = 50, step = 1),
+      selectInput("Nicotine", "Smoking Status", 
+                  choices = c("Never" = "Never", "Current" = "Current", "Former" = "Former"), selected = "Never"),
+      selectInput("Cholesterol", "Cholesterol Status", 
+                  choices = c("No" = "No", "Yes" = "Yes"), selected = "No"),
+      selectInput("DM", "Diabetes Mellitus", 
+                  choices = c("No" = "No", "Yes" = "Yes"), selected = "No"),
+      selectInput("BPQ050A", "Hypertension Treatment", 
+                  choices = c("No" = "No", "Yes" = "Yes"), selected = "No"),
+      selectInput("ACES_with_complexity_combined", "ACES Classification",
+                  choices = c("Other" = "Other", "Stage III" = "Stage III", 
+                              "Stage IV" = "Stage IV", "Edentulous" = "Edentulous"), selected = "None"),
+      actionButton("calculate", "Calculate Likelihood")
+    ),
+    
+    mainPanel(
+      h4("Nomogram Plot"),
+      plotOutput("nomogramPlot"),
+      h4("Predicted Probability of CVD"),
+      textOutput("predictedProbability")
+    )
+  )
+)
